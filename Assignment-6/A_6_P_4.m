5a4b341ee3c8fb793702d19acc7d562f4f8f971e
Chen-Jialin <49650983+Chen-Jialin@users.noreply.github.com>
2020-11-12 01:04:11 +0800

    A6 correction.

--- Assignment-6/A_6_P_4.m
+++ Assignment-6/A_6_P_4.m
@@ -3,17 +3,17 @@ N = 8;
 A = 2;
 W = 1;
 W_g = 3 * W;
-f_1 = 2 * W + W_g;
-f = -40:0.01:40;
-S_c = (f >= -W) .* (f <= W);
-for k = 1:(N - 1)
-    S_c = S_c + k^2 * ((0.5 * (f >= (k * f_1)) .* (f <= (W + k * f_1))...
+f_1 = W + W_g;
+f = -30:0.01:30;
+S_c = 0;
+for k = 0:(N - 1)
+    S_c = S_c + (48 * k^2 + 12 * k + 1) * ((0.5 * (f >= (k * f_1)) .* (f <= (W + k * f_1))...
         + 0.5 .* (f >= (- W - k * f_1)) .* (f <= (- k * f_1))));
 end
 S_c = A * S_c;
 plot(f, S_c)
 hold on
-S_n = A / 500 * f.^2;
+S_n = A / 10 * f.^2;
 plot(f, S_n, '--')
 xlim([f(1), f(end)])
 xtick = [];
@@ -28,7 +28,7 @@ for k = 1:(N - 1)
     xtick(n) = f;
     xticklabel(n) = {[num2str(f), 'W']};
     n = n + 1;
-    f = f + W + W_g;
+    f = f + W_g;
 end
 xtick(n) = f;
 xticklabel(n) = {'-W'};
@@ -37,7 +37,7 @@ f = f + 2 * W;
 xtick(n) = f;
 xticklabel(n) = {'W'};
 n = n + 1;
-f = f + W_g + W;
+f = f + W_g;
 for k = 1:(N - 1)
     xtick(n) = f;
     xticklabel(n) = {[num2str(f), 'W']};
@@ -46,7 +46,7 @@ for k = 1:(N - 1)
     xtick(n) = f;
     xticklabel(n) = {[num2str(f), 'W']};
     n = n + 1;
-    f = f + W + W_g;
+    f = f + W_g;
 end
 set(gca,'xtick', xtick, 'xticklabel', xticklabel, 'fontsize', 6)
 ytick = [0];
